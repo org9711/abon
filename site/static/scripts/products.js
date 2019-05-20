3bc4111e1f0a7094c8f35e2221d48cfc4fe68f3b
kimdogbe <kimdogbe@gmail.com>
2019-05-20 13:20:31 +0100

    basket table now works with server

--- site/static/scripts/products.js
+++ site/static/scripts/products.js
@@ -36,15 +36,56 @@ function displayProducts() {
   console.log(products);
   let ul = document.querySelector("#product-list");
   for (var i = 0; i < products.length; i++) {
+      let productId = products[i].id;
+      let productName = products[i].name;
+      let productPrice = products[i].price;
       let product = productLayout.cloneNode(true);
       let name = product.getElementsByTagName('h1')[0];
       let image = product.getElementsByClassName('product-image')[0];
+      let buttonDiv = product.getElementsByClassName('carousel-caption mask proudct-content')[0];
       name.textContent = products[i].name;
       image.src = products[i].image_name;
+      
+      let addToBasketButton = document.createElement("button");
+      addToBasketButton.innerHTML = "Add to Basket";
+      addToBasketButton.className = "mt-1 btn btn-sm btn-info";
+      addToBasketButton.addEventListener("click", function(){
+            var myCart = document.getElementById("cart");   
+            addToBasket(myCart, productId, productName, productPrice);
+      });
+      buttonDiv.append(addToBasketButton);
+      
       product = product.firstElementChild;
-      console.log(product);
       ul.appendChild(product);
   }
+}
+
+function addToBasket(myCart, productId, productName, productPrice) { 
+        
+        var rowId = "row-id-" + productId.toString();
+        var meal = myCart.getElementsByClassName(rowId)[0];    
+        
+        if(meal == null) {
+            let el = document.createElement('html');
+            
+            
+            rowText = '<tr class="' + rowId + '"><td class="pt-3-half">' + productName + '</td><td class="pt-3-half">' + productPrice + '</td><td class="pt-3-half meal-quantity" contenteditable="false">1</td><td><button type="button" class="btn btn-danger btn-rounded removeBtn btn-sm my-0">Remove</button></td></tr>';
+            
+            el.innerHTML = rowText;
+            rowHTML = el.firstElementChild;
+            myCart.append(rowHTML);
+                        
+            meal = myCart.getElementsByClassName(rowId)[0];
+            
+            //remove button removes row
+            meal.getElementsByClassName("removeBtn")[0].addEventListener("click", function(){
+                this.parentElement.parentElement.remove();
+            });
+        }
+    else {
+            var mealQuantity = meal.getElementsByClassName('pt-3-half meal-quantity')[0];
+            mealQuantity.innerHTML = parseInt(mealQuantity.innerHTML) + 1;
+        }
 }
 
 function getHeader() {
