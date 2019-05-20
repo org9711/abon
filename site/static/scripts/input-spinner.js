813ebfcd0672e3abccc59f3b3bd34ee4bf5950f6
kimdogbe <kimdogbe@gmail.com>
2019-05-20 16:16:30 +0100

    got freaking breakpoints working

--- site/static/scripts/input-spinner.js
+++ site/static/scripts/input-spinner.js
@@ -1,4 +1,4 @@
-document.onclick('click', '.number-spinner button', function () {    
+$(document).on('click', '.number-spinner button', function () {    
 	var btn = $(this),
 		oldValue = btn.closest('.number-spinner').find('input').val().trim(),
 		newVal = 0;
