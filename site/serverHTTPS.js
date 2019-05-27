d91c80d00b8e11b74277202581327f9dabc64344
Oliver Ryan-George <org.9711@hotmail.co.uk>
2019-05-27 05:30:31 +0100

    more cleaning

--- site/serverHTTPS.js
+++ site/serverHTTPS.js
@@ -1,13 +1,17 @@
 let HTTPS = require('https');
 let fs = require('fs');
-let OK = 200, NotFound = 404, BadType = 415;
+let OK = 200, Unauthorized = 401, NotFound = 404, BadType = 415;
 let respond = require('./lib/respond.js');
-let htmlsC = require('./controllers/htmls.js');
-let scriptsC = require('./controllers/scripts.js');
+let security = require('./lib/security.js');
+let indexC = require('./controllers/index.js');
+let filesC = require('./controllers/files.js');
 let picturesC = require('./controllers/pictures.js');
 let productsC = require('./controllers/products.js');
 let testimonialsC = require('./controllers/testimonials.js');
+let aboutC = require('./controllers/about.js');
 let frameC = require('./controllers/frame.js');
+let adminC = require('./controllers/admin.js');
+let loginC = require('./controllers/login.js');
 start(44300);
 
 // Provide a service to localhost only.
@@ -22,38 +26,41 @@ function start(port) {
   console.log("Visit localhost:" + port);
 }
 
+
 // Deal with a request.
 async function handle(request, response) {
-  await validateURL(request, response);
-  if (request.url.endsWith(".png") || request.url.endsWith(".jpg")) {
-    picturesC.handle(request, response);
-  }
-  else if (request.url.endsWith(".js") || request.url.endsWith(".css")) {
-    scriptsC.handle(request, response);
-  }
-  else if (request.url.startsWith("/frame")) {
-    frameC.handle(request, response);
-  }
-  else if (request.url.startsWith("/products")) {
-    productsC.handle(request, response);
-  }
-  else if (request.url.startsWith("/testimonials")) {
-    testimonialsC.handle(request, response);
-  }
-  else if (request.url.startsWith("/")) {
-    htmlsC.handle(request, response);
+  await security.validateURL(request, response);
+  if (request.url.startsWith("/admin")) {
+    adminC.handle(request, response);
   }
   else {
-    return respond.fail(response, NotFound, "Request URL not valid.");
-  }
-}
-
-async function validateURL(request, response) {
-  dotDot = request.url.includes("..");
-  slashSlash = request.url.includes("//");
-  dotSlash = request.url.includes("./");
-  slashDot = request.url.includes("/.");
-  if (dotDot || slashSlash || dotSlash || slashDot) {
-    return respond.fail(response, BadType, "Restricted characters used in URL.")
+    if (request.url.startsWith("/favicon.ico")) request.url = 'square.png';
+    if (request.url.endsWith(".png") || request.url.endsWith(".jpg")) {
+      picturesC.handle(request, response);
+    }
+    else if (request.url.endsWith(".js") || request.url.endsWith(".css") || request.url.endsWith(".html")) {
+      filesC.handle(request, response);
+    }
+    else if (request.url.startsWith("/frame")) {
+      frameC.handle(request, response);
+    }
+    else if (request.url.startsWith("/products")) {
+      productsC.handle(request, response);
+    }
+    else if (request.url.startsWith("/testimonials")) {
+      testimonialsC.handle(request, response);
+    }
+    else if (request.url.startsWith("/about")) {
+      aboutC.handle(request, response);
+    }
+    else if (request.url.startsWith("/login")) {
+      loginC.handle(request, response);
+    }
+    else if (request.url == "/") {
+      indexC.handle(request, response);
+    }
+    else {
+      return respond.fail(response, NotFound, "Request URL not valid.");
+    }
   }
 }
