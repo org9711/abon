04850f87aeaa072b253a411a5a8f5ba181a5ab77
Oliver Ryan-George <org.9711@hotmail.co.uk>
2019-08-30 00:17:46 +0100

    initial content insertion

---
+++ site/client/public/scripts/product_display.js
@@ -0,0 +1,53 @@
+let productLayout;
+
+function getProductLayout() {
+  let q = new XMLHttpRequest();
+  q.onreadystatechange = storeProductLayout;
+  q.open("GET", '/products/get_product_layout', true);
+  q.send();
+}
+
+function storeProductLayout() {
+  if(this.readyState != XMLHttpRequest.DONE) return;
+  el = document.createElement("html");
+  el.innerHTML = this.responseText;
+  productLayout = el;
+  getProducts();
+}
+
+function getProducts() {
+  let q = new XMLHttpRequest();
+  q.onreadystatechange = displayProducts;
+  q.open("GET", '/products/get_products', true);
+  q.send();
+}
+
+function displayProducts() {
+  if (this.readyState != XMLHttpRequest.DONE) return;
+  products = JSON.parse(this.responseText);
+  let ul = document.getElementById("products");
+
+  for (let i = 0; i < products.length; i++) {
+    let id = products[i].id;
+    let name = products[i].name;
+    let price = products[i].price;
+    let imageName = products[i].image_name;
+    let status = products[i].status;
+    let description = products[i].description;
+
+    let productDiv = productLayout.cloneNode(true);
+
+    let nameTag = productDiv.querySelector("div h3");
+    let imageTag = productDiv.querySelector("div img");
+
+    nameTag.textContent = name;
+    imageTag.src = imageName;
+
+    console.log(imageTag);
+    console.log(nameTag);
+
+    productDiv = productDiv.firstElementChild;
+
+    ul.append(productDiv);
+  }
+}
