8fc86d993641d6209f801d065e669a9e870b730e
Oliver Ryan-George <org.9711@gmail.com>
2020-02-04 20:41:24 +0000

    navigation added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomePage } from './pages/home/home.component';
+import { ProductsPage } from './pages/products/products.component';
+import { TestimonialsPage } from './pages/testimonials/testimonials.component';
+import { AboutPage } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'products', component: ProductsPage },
+  { path: 'testimonials', component: TestimonialsPage },
+  { path: 'about', component: AboutPage }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BurgerComponent } from './components/navbar/burger/burger.component';
+import { HomePage } from './pages/home/home.component';
+import { ProductsPage } from './pages/products/products.component';
+import { TestimonialsPage } from './pages/testimonials/testimonials.component';
+import { AboutPage } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BurgerComponent
+    BurgerComponent,
+    HomePage,
+    ProductsPage,
+    TestimonialsPage,
+    AboutPage
   ],
   imports: [
     BrowserModule,
